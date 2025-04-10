8b05fa2f8ef2e4befef320b5a1d260b72e1bf6ca
SnarglePop <71835329+SnarglePop@users.noreply.github.com>
2025-04-10 15:03:01 +0800

    changed testing methods, changed to window

--- python/svm_rf.py
+++ python/svm_rf.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 from multiprocessing import Pool
 from functools import partial
 from sklearn.svm import SVC
+from matplotlib import pyplot as plt
+from datetime import datetime
+import scipy.signal
 
 def extract_npy(filepath: str):
     """
@@ -48,8 +51,8 @@ if __name__ == "__main__":
     d = 6 # LBP bit size
     num_chs = 17 # constant
     levels = 64
-    training_patients = ["chb"+str(x).zfill(2) for x in range(10, 10+1)]
-    testing_patients = ["chb"+str(x).zfill(2) for x in range(10, 10+1)]
+    training_patients = ["chb"+str(x).zfill(2) for x in range(1, 1+1)]
+    testing_patients = ["chb"+str(x).zfill(2) for x in range(1, 1+1)]
     training_features = [1,2,3]
     feature_array = np.array([])
     label_array = np.array([])
@@ -60,41 +63,61 @@ if __name__ == "__main__":
 
     # 1 - line length and mean amp
     # 2 - spectral power
+    log_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     with open("svm_train_log.txt", 'a') as log:
             log.write(dedent(
                 f"""
                 ---
-                Test Begin
+                Test Begin {log_time}
                 ---
                 #1 - Line Length + Mean Amplitude
                 #2 - Band Power
                 #3 - LBP
                 """
             ))
-    print("#1 - Line Length + Mean Amplitude")
-    print("#2 - Band Power")
+    # b_filt, a_filt = scipy.signal.iirfilter(4, Wn=45, fs=fs, btype="low", ftype="butter")
+    # h_filt = scipy.signal.firwin(5, 45, fs=fs)
 
 
     for feature in training_features:
         for patient in training_patients:
-            for label_exc in ["non-seizures", "seizures"]:
+            label_exc = "non-seizures"
+            path_patient = f"chbmit-eeg-processed/{label_exc}/" + patient
+            files_patient = os.listdir(path_patient)
+            files = len(files_patient)
+            for exc in range(files):
+                label_exc = "non-seizures"
                 path_patient = f"chbmit-eeg-processed/{label_exc}/" + patient
                 files_patient = os.listdir(path_patient)
+                file_exc = np.array([])
+                file_exc = np.append(file_exc, files_patient[exc])
+                label_exc = "seizures"
+                path_patient = f"chbmit-eeg-processed/{label_exc}/" + patient
+                files_patient = os.listdir(path_patient)
+                file_exc = np.append(file_exc, files_patient[exc])
+                feature_array = np.array([])
+                label_array = np.array([])
+                feature_array_test = np.array([])
+                label_array_test = np.array([])
 
-                for file_exc in files_patient:
-                    feature_array = np.array([])
-                    label_array = np.array([])
-                    feature_array_test = np.array([])
-                    label_array_test = np.array([])
-
-                    for label in ["non-seizures", "seizures"]:
-                        for file_inc in files_patient:
-                            if file_inc == file_exc:
-                                continue
-                            else:
-                                name_chs, value_chs, last_samp, samp_freq = extract_npy(str(path_patient + "/"+ file_inc))
-                                for i in value_chs:
+                for label in ["seizures", "non-seizures"]:
+                    path_patient = f"chbmit-eeg-processed/{label}/" + patient
+                    files_patient = os.listdir(path_patient)
+                    files = len(files_patient)
+                    for file_inc in files_patient:
+                        if file_inc in file_exc:
+                            continue
+                        else:
+                            name_chs, value_chs, last_samp, samp_freq = extract_npy(str(path_patient + "/"+ file_inc))
+                            for m in value_chs:
                                 # line length and mean amplitude
+                                # i = scipy.signal.filtfilt(b_filt, a_filt, i)
+                                # i = scipy.signal.upfirdn(h_filt, i)
+                                for j in range(0, int(len(m)), window_step):
+                                    if j + window_size >= len(m):
+                                        break
+                                    else:
+                                        i = m[j:j+window_size]
                                     if feature == 1:
                                         ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
                                         feature_array = np.append(feature_array, ll_ma)
@@ -105,55 +128,100 @@ if __name__ == "__main__":
                                         lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
                                         feature_array = np.append(feature_array, lbp)
                                     if label == "non-seizures":
-                                        label_array = np.append(label_array, [0])
+                                        label_array = np.append(label_array, [-1])
+                                        
                                     else:
                                         label_array = np.append(label_array, [1])
-
-                    print("Completed training leave one out for"+ file_exc)
-                    if feature == 1:
-                        feature_array = feature_array.reshape(-1,2)
-                    if feature == 2:
-                        feature_array = feature_array.reshape(-1,6)
-                    if feature == 3:
-                        feature_array = feature_array.reshape(-1,64)
-                    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-                    clf.fit(feature_array, label_array)
-
-                    name_chs, value_chs, last_samp, samp_freq = extract_npy(str(path_patient + "/"+ file_exc))
-                    for i in value_chs:
-                        # line length and mean amplitude
-                        if feature == 1:
-                            ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
-                            feature_array_test = np.append(feature_array_test, ll_ma)
-                        if feature == 2:
-                            sp = np.array(hdc.compute_bandPower(i, fs, window_size))
-                            feature_array_test = np.append(feature_array_test, sp)
-                        elif feature == 3:
-                            lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
-                            feature_array_test = np.append(feature_array_test, lbp)
-                        if label == "non-seizures":
-                            label_array_test = np.append(label_array_test, [0])
-                        else:
-                            label_array_test = np.append(label_array_test, [1])
-                    print("Completed testing " + file_exc)
-
-                    if feature == 1:
-                        feature_array_test = feature_array_test.reshape(-1,2)
-                    if feature == 2:
-                        feature_array_test = feature_array_test.reshape(-1,6)
-                    if feature == 3:
-                        feature_array_test = feature_array_test.reshape(-1,64)
-
-                    print("For feature #"+str(feature)+" and for file " + file_exc + ", for " + label)
-                    accuracy = clf.score(feature_array_test, label_array_test)
-                    print(accuracy)
-                    with open("svm_train_log.txt", 'a') as log:
-                        log.write(dedent(
-                            f"""
-                            Feature #{feature} with {label} file {file_exc} - {accuracy}
-                            """
-                    ))
+                                    
                 
+                if feature == 1:
+                    feature_array = feature_array.reshape(-1,2)
+                if feature == 2:
+                    feature_array = feature_array.reshape(-1,6)
+                if feature == 3:
+                    feature_array = feature_array.reshape(-1,64)
+                clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+                clf.fit(feature_array, label_array)
+                print(f"Completed training leave one out for {file_exc} with length of {len(feature_array)}")
+                for file_exc_iter in range(len(file_exc)):
+                    if file_exc_iter:
+                        path_patient = f"chbmit-eeg-processed/seizures/" + patient
+                        label_exc = "seizures"
+                    else:
+                        path_patient = f"chbmit-eeg-processed/non-seizures/" + patient
+                        label_exc = "non-seizures"
+                    file_exc_name = file_exc[file_exc_iter]
+                    name_chs, value_chs, last_samp, samp_freq = extract_npy(str(path_patient + "/"+ file_exc_name))
+                    for m in value_chs:
+                        # line length and mean amplitude
+                        for j in range(0, int(len(m)), window_step):
+                            if j + window_size >= len(m):
+                                break
+                            else:
+                                i = m[j:j+window_size]
+                            if feature == 1:
+                                ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
+                                feature_array_test = np.append(feature_array_test, ll_ma)
+                            if feature == 2:
+                                sp = np.array(hdc.compute_bandPower(i, fs, window_size))
+                                feature_array_test = np.append(feature_array_test, sp)
+                            elif feature == 3:
+                                lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
+                                feature_array_test = np.append(feature_array_test, lbp)
+                            if label_exc == "non-seizures":
+                                label_array_test = np.append(label_array_test, [-1])
+                            else:
+                                label_array_test = np.append(label_array_test, [1])
+                print(f"Completed testing {file_exc}")
+
+                if feature == 1:
+                    feature_array_test = feature_array_test.reshape(-1,2)
+                if feature == 2:
+                    feature_array_test = feature_array_test.reshape(-1,6)
+                if feature == 3:
+                    feature_array_test = feature_array_test.reshape(-1,64)
+                
+                
+
+                print(f"For feature #{feature} and for file {file_exc}")
+                sens = 0
+                spec = 0
+                sens_tot = 0
+                spec_tot = 0
+                test_out = clf.predict(feature_array_test)
+                for l in range(len(test_out)):
+                    t = test_out[l]
+                    if label_array_test[l] == -1:
+                        spec_tot += 1
+                        if t == -1:
+                            spec += 1
+                    else:
+                        sens_tot += 1
+                        if t == 1:
+                            sens += 1
+                print(f"Seizures: {sens} out of {sens_tot}")
+                print(f"Non-seizures: {spec} out of {spec_tot}")
+
+                accuracy = clf.score(feature_array_test, label_array_test)
+                print(accuracy)
+                with open("svm_train_log.txt", 'a') as log:
+                    log.write(dedent(
+                        f"""
+                        Feature #{feature} with {label_exc} file {file_exc} - {accuracy}
+                        Seizures: {sens} out of {sens_tot}
+                        Non-seizures: {spec} out of {spec_tot}
+                        """
+                ))
+    log_time = datetime.today().strftime('%H:%M:%S')
+    with open("svm_train_log.txt", 'a') as log:
+            log.write(dedent(
+                f"""
+                ---
+                Test End - {log_time}
+                
+                """
+            ))
+            
                             
 
 
