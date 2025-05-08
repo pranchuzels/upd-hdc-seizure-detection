57f9ab3c75c2fd81b2970cea5b66a9534725d9c0
SnarglePop <71835329+SnarglePop@users.noreply.github.com>
2025-05-09 02:25:17 +0800

    Update postproc_test.py

--- python/postproc_test.py
+++ python/postproc_test.py
@@ -7,31 +7,36 @@ from multiprocessing import Pool
 from functools import partial
 
 if __name__ == "__main__":
-    for feat in ["feature1","feature2","feature3","feature4"]:
-        sens = 0
-        spec = 0
-        sens_tot = 0
-        spec_tot = 0
-        for type in ["seizures", "nonseizures"]:
-            folderin = f"preds_hdc/postproc/{type}/"
-            for file in os.listdir(folderin):
-                if type == "seizures":
-                    patient, feature, file_id, _ = file.split("_")
-                else:
-                    patient, feature, file_id = file.split("_")
-                if feature != feat:
-                    continue
-                testarr = np.loadtxt(folderin+file)
-                for val in testarr:
-                    if type == "seizures":
-                        testval = 1
-                        sens_tot += 1
-                        if val == testval:
-                            sens += 1
-                    else:
-                        testval = -1
-                        spec_tot += 1
-                        if val == testval:
-                            spec += 1
-        print(f"Sensitivity for {feat}: {sens} out of {sens_tot} - {(sens/sens_tot)*100}%")
-        print(f"Specificity for {feat}: {spec} out of {spec_tot} - {(spec/spec_tot)*100}%")
+    with open("testing_log.txt", 'a') as log:
+        log.write(dedent("Test Start"))
+    for patient in ["chb"+str(x).zfill(2) for x in range(1, 24+1)]:
+        for feat in ["feature2","feature4"]:
+            sens = 0
+            spec = 0
+            sens_tot = 0
+            spec_tot = 0
+            for type in ["seizures", "non-seizures"]:
+                folderin = f"post/{type}/"
+                for file in os.listdir(folderin):
+                    curpatient, feature, file_id, _ = file.split("_")
+                    if feature != feat or curpatient != patient:
+                        continue
+                    testarr = np.loadtxt(folderin+file, ndmin=1)
+                    for val in testarr:
+                        if type == "seizures":
+                            testval = 1
+                            sens_tot += 1
+                            if val == testval:
+                                sens += 1
+                        else:
+                            testval = -1
+                            spec_tot += 1
+                            if val == testval:
+                                spec += 1
+            with open("testing_log_hdc_post.txt", 'a') as log:
+                log.write(dedent(f"""
+                    Sensitivity for {patient} {feat}: {sens} out of {sens_tot} - {(sens/sens_tot)*100}%
+                    Specificity for {patient} {feat}: {spec} out of {spec_tot} - {(spec/spec_tot)*100}%
+                """))
+    with open("testing_log.txt", 'a') as log:
+        log.write(dedent("Test End"))
