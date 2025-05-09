d8db42d5fffa003b77b5c9db91693698d8281bda
SnarglePop <71835329+SnarglePop@users.noreply.github.com>
2025-05-09 11:15:48 +0800

    correct features

--- python/svm_rf.py
+++ python/svm_rf.py
@@ -42,6 +42,19 @@ def extract_npy(filepath: str):
     
     return label_chs, value_chs, int(last_samp), int(samp_freq)
 
+def post_processing_window(arr, tolerance: int):
+    for i in range(len(arr)):
+        if i - tolerance < 0 or i + tolerance >= len(arr):
+            continue
+        else:
+            arr1 = np.array(arr[i-tolerance:i])
+            arr2 = np.array(arr[i+1:i+tolerance+1])
+            # print(f"{arr1} - {arr[i]} - {arr2}")
+            if np.array_equal(arr1, arr2):
+                # print("changed")
+                arr[i] = arr[i-tolerance]
+    return arr
+
 if __name__ == "__main__":
     # parameters
     n = 10000 # dimension of HV
@@ -51,18 +64,18 @@ if __name__ == "__main__":
     d = 6 # LBP bit size
     num_chs = 17 # constant
     levels = 64
-    training_patients = ["chb"+str(x).zfill(2) for x in range(1, 1+1)]
-    testing_patients = ["chb"+str(x).zfill(2) for x in range(1, 1+1)]
-    training_features = [1,2,3]
+    training_patients = ["chb"+str(x).zfill(2) for x in range(16, 24+1)]
+    testing_patients = ["chb"+str(x).zfill(2) for x in range(2, 1+1)]
+    training_features = [3,4]
     feature_array = np.array([])
     label_array = np.array([])
     feature_array_test = np.array([])
     label_array_test = np.array([])
+    use_post_processing = False
+    tolerance = 5
 
     # TRAINING PHASE
 
-    # 1 - line length and mean amp
-    # 2 - spectral power
     log_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     with open("svm_train_log.txt", 'a') as log:
             log.write(dedent(
@@ -70,9 +83,11 @@ if __name__ == "__main__":
                 ---
                 Test Begin {log_time}
                 ---
-                #1 - Line Length + Mean Amplitude
+                #1 - LBP
                 #2 - Band Power
-                #3 - LBP
+                #3 - LBP + LL + MA
+                #4 - LL + SP
+                Postprocessing is set to {use_post_processing}
                 """
             ))
     # b_filt, a_filt = scipy.signal.iirfilter(4, Wn=45, fs=fs, btype="low", ftype="butter")
@@ -80,11 +95,23 @@ if __name__ == "__main__":
 
 
     for feature in training_features:
+        with open("svm_train_log.txt", 'a') as log:
+                log.write(dedent(
+                    f"""
+                    Feature #{feature}
+                    """
+            ))
         for patient in training_patients:
             label_exc = "non-seizures"
             path_patient = f"chbmit-eeg-processed/{label_exc}/" + patient
             files_patient = os.listdir(path_patient)
             files = len(files_patient)
+            sens_tot_tot = 0
+            spec_tot_tot = 0
+            avg_sens = 0
+            avg_spec = 0
+            avg_sens_0 = 0
+            avg_spec_0 = 0
             for exc in range(files):
                 label_exc = "non-seizures"
                 path_patient = f"chbmit-eeg-processed/{label_exc}/" + patient
@@ -109,24 +136,34 @@ if __name__ == "__main__":
                             continue
                         else:
                             name_chs, value_chs, last_samp, samp_freq = extract_npy(str(path_patient + "/"+ file_inc))
+                            counter = 0
                             for m in value_chs:
+                                counter += 1
                                 # line length and mean amplitude
                                 # i = scipy.signal.filtfilt(b_filt, a_filt, i)
                                 # i = scipy.signal.upfirdn(h_filt, i)
+                                print(f"Working... {counter} of {len(value_chs)} - {file_inc}")
                                 for j in range(0, int(len(m)), window_step):
                                     if j + window_size >= len(m):
                                         break
                                     else:
                                         i = m[j:j+window_size]
                                     if feature == 1:
-                                        ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
-                                        feature_array = np.append(feature_array, ll_ma)
+                                        lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
+                                        feature_array = np.append(feature_array, lbp)
                                     elif feature == 2:
                                         sp = np.array(hdc.compute_bandPower(i, fs, window_size))
                                         feature_array = np.append(feature_array, sp)
                                     elif feature == 3:
                                         lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
                                         feature_array = np.append(feature_array, lbp)
+                                        ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
+                                        feature_array = np.append(feature_array, ll_ma)
+                                    elif feature == 4:
+                                        sp = np.array(hdc.compute_bandPower(i, fs, window_size))
+                                        feature_array = np.append(feature_array, sp)
+                                        ll = np.array([hdc.compute_lineLength(i)])
+                                        feature_array = np.append(feature_array, ll)
                                     if label == "non-seizures":
                                         label_array = np.append(label_array, [-1])
                                         
@@ -135,11 +172,13 @@ if __name__ == "__main__":
                                     
                 
                 if feature == 1:
-                    feature_array = feature_array.reshape(-1,2)
+                    feature_array = feature_array.reshape(-1,64)
                 if feature == 2:
                     feature_array = feature_array.reshape(-1,6)
                 if feature == 3:
-                    feature_array = feature_array.reshape(-1,64)
+                    feature_array = feature_array.reshape(-1,66)
+                if feature == 4:
+                    feature_array = feature_array.reshape(-1,7)
                 clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
                 clf.fit(feature_array, label_array)
                 print(f"Completed training leave one out for {file_exc} with length of {len(feature_array)}")
@@ -160,35 +199,51 @@ if __name__ == "__main__":
                             else:
                                 i = m[j:j+window_size]
                             if feature == 1:
-                                ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
-                                feature_array_test = np.append(feature_array_test, ll_ma)
-                            if feature == 2:
+                                lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
+                                feature_array_test = np.append(feature_array_test, lbp)
+                            elif feature == 2:
                                 sp = np.array(hdc.compute_bandPower(i, fs, window_size))
                                 feature_array_test = np.append(feature_array_test, sp)
                             elif feature == 3:
                                 lbp = np.histogram(hdc.compute_optimizedLBP(i,6),bins=np.array(range(0,65)))[0]
                                 feature_array_test = np.append(feature_array_test, lbp)
+                                ll_ma = np.array([hdc.compute_lineLength(i),hdc.compute_meanAmp(i)])
+                                feature_array_test = np.append(feature_array_test, ll_ma)
+                            elif feature == 4:
+                                sp = np.array(hdc.compute_bandPower(i, fs, window_size))
+                                feature_array_test = np.append(feature_array_test, sp)
+                                ll = np.array([hdc.compute_lineLength(i)])
+                                feature_array_test = np.append(feature_array_test, ll)
                             if label_exc == "non-seizures":
                                 label_array_test = np.append(label_array_test, [-1])
                             else:
                                 label_array_test = np.append(label_array_test, [1])
                 print(f"Completed testing {file_exc}")
-
                 if feature == 1:
-                    feature_array_test = feature_array_test.reshape(-1,2)
+                    feature_array_test = feature_array_test.reshape(-1,64)
                 if feature == 2:
                     feature_array_test = feature_array_test.reshape(-1,6)
                 if feature == 3:
-                    feature_array_test = feature_array_test.reshape(-1,64)
+                    feature_array_test = feature_array_test.reshape(-1,66)
+                if feature == 4:
+                    feature_array_test = feature_array_test.reshape(-1,7)
                 
                 
 
                 print(f"For feature #{feature} and for file {file_exc}")
+                
                 sens = 0
                 spec = 0
                 sens_tot = 0
                 spec_tot = 0
                 test_out = clf.predict(feature_array_test)
+                if use_post_processing:
+                    print(f"Post-processing enabled")
+                    test_out = post_processing_window(test_out, tolerance)
+                    pred_filename = f"predictions/postprocessed/Feature{feature}_{file_exc[0]}.txt"
+                else:
+                    pred_filename = f"predictions/nonpost/Feature{feature}_{file_exc[0]}.txt"
+                np.savetxt(pred_filename, test_out)
                 for l in range(len(test_out)):
                     t = test_out[l]
                     if label_array_test[l] == -1:
@@ -200,18 +255,26 @@ if __name__ == "__main__":
                         if t == 1:
                             sens += 1
                 print(f"Seizures: {sens} out of {sens_tot}")
+                avg_sens += sens
+                sens_tot_tot += sens_tot
                 print(f"Non-seizures: {spec} out of {spec_tot}")
+                avg_spec += spec
+                spec_tot_tot += spec_tot
+            avg_sens_0 = avg_sens/sens_tot_tot
+            avg_spec_0 = avg_spec/spec_tot_tot
+            print(f"Sensitivity of {patient}: {avg_sens_0}")
+            print(f"Specificity of {patient}: {avg_spec_0}")
+            with open("svm_train_log.txt", 'a') as log:
+                log.write(dedent(
+                    f"""
+                    {patient}
+                    Sensitivity: {avg_sens_0} ({avg_sens} out of {sens_tot_tot})
+                    Specificity: {avg_spec_0} ({avg_spec} out of {spec_tot_tot})
+                    """
+            ))
+            
 
-                accuracy = clf.score(feature_array_test, label_array_test)
-                print(accuracy)
-                with open("svm_train_log.txt", 'a') as log:
-                    log.write(dedent(
-                        f"""
-                        Feature #{feature} with {label_exc} file {file_exc} - {accuracy}
-                        Seizures: {sens} out of {sens_tot}
-                        Non-seizures: {spec} out of {spec_tot}
-                        """
-                ))
+
     log_time = datetime.today().strftime('%H:%M:%S')
     with open("svm_train_log.txt", 'a') as log:
             log.write(dedent(
